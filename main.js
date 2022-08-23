ec03b97214e6c1cfbd04b8ed3a4ca554ebb32227
Christin Schade <christinabbe90gmail.com>
2022-08-23 14:20:35 +0200

    feat: create countdown

---
+++ main.js
@@ -0,0 +1,21 @@
+const countDownDate = new Date('Sep 15, 2022 07:15:00').getTime();
+
+let x = setInterval(function() {
+
+  let now = new Date().getTime();
+
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 *60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
